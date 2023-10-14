f43cf35314ee04bce2724f9e9178ffe3c9ab829d
Arnold120 <146139954+Arnold120@users.noreply.github.com>
2023-10-14 17:00:25 -0600

    Prueba de prueba

---
+++ SistemaDefacturacion.cs
@@ -0,0 +1,40 @@
+using SistemaDeFacturacion.Entidades;
+
+public class SitemaDeFacturacion
+{
+    public static void Main()
+     {
+        FacturaEskimo factura = new FacturaEskimo("Jose Martin", 1000);
+        // MostrarInformacion(factura);
+
+        MostrarInformacion(factura);
+
+        factura.RegistrarPago(300, DateTime.UtcNow, "Compra de super eskimo");
+        factura.RegistrarPago(120, DateTime.UtcNow, "Compra de un fruta rellena ");
+        try
+        {
+            factura.RegistrarPago(130, DateTime.UtcNow, "Compra de ron con pasa");
+        }
+
+        catch (Exception exception)
+
+        {
+            Console.WriteLine("Excepcion Capturada: " + exception.Message);
+        }
+
+        MostrarInformacion(factura);
+    }
+
+ 
+
+    public static void MostrarInformacion(FacturaEskimo factura)
+    {
+
+        Console.WriteLine("===================== Informacion de la factura =====================");
+        Console.WriteLine("Numero de factura: " + factura.Numero);
+        Console.WriteLine("Cliente: " + factura.Cliente);
+        Console.WriteLine("Total: " + factura.Total);
+        // Console.WriteLine("===================== Fin =====================");
+
+    }
+}
